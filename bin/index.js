c64fc8d94fdbf3702a6b83803331a4727301c11a
Abdur Rahman <abdurrahman.abdullahalbasir@ge.com>
2017-11-20 13:14:41 -0500

    Update transpilation steps, add minification

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,3 @@
 #!/usr/bin/env node
-
-// @flow
-"use strict";
-
-const { promisify } = require("bluebird");
-const params = require("ara-cli-options");
-const { readFile, writeFile } = require("fs");
-const __readFile = promisify(readFile);
-const __writeFile = promisify(writeFile);
-const parser = new require("xml2js").Parser();
-const parseString = promisify(parser.parseString);
-const DIR = `${process.env.PWD}/coverage`;
-
-if (process.argv[2] === "help" || process.argv[2] === "-h" || process.argv[2] === "--help") {
-	displayUsage();
-}
-
-const CLOVER_COVERAGE_LOCATION = params("--location", "-l") || `${DIR}`;
-const runStyle = params("--style", "-s") || "coverage";
-const generateGenericTestData = require(`./${runStyle}`);
-
-__readFile(`${CLOVER_COVERAGE_LOCATION}/clover.xml`)
-	.then(fileContent => parseString(fileContent))
-	.then(parsedFileContent => generateGenericTestData(parsedFileContent))
-	.then(fileOutputContent => __writeFile(`${CLOVER_COVERAGE_LOCATION}/sonar-report.xml`, fileOutputContent))
-	.then(() => console.info(`Successfully wrote coverage to ${CLOVER_COVERAGE_LOCATION}/sonar-report.xml`))
-	.catch(err => {
-		console.error(err);
-		process.exit(1);
-	});
-
-function displayUsage() {
-	console.info(`-l --location: [./coverage]
-	             -s --style: [coverage]||execution`);
-	process.exit(0);
-}
+"use strict";const{promisify}=require("util");const params=require("ara-cli-options");const{readFile,writeFile}=require("fs");const __readFile=promisify(readFile);const __writeFile=promisify(writeFile);const parser=new require("xml2js").Parser();const parseString=promisify(parser.parseString);const DIR=`${process.env.PWD}/coverage`;if(process.argv[2]==="help"||process.argv[2]==="-h"||process.argv[2]==="--help"){displayUsage()}const CLOVER_COVERAGE_LOCATION=params("--location","-l")||`${DIR}`;const runStyle=params("--style","-s")||"coverage";const generateGenericTestData=require(`./${runStyle}`);(async()=>{const fileContent=await __readFile(`${CLOVER_COVERAGE_LOCATION}/clover.xml`);const parsedFileContent=await parseString(fileContent);const fileOutputContent=await generateGenericTestData(parsedFileContent);const result=await __writeFile(`${CLOVER_COVERAGE_LOCATION}/sonar-report.xml`,fileOutputContent);console.info(`Successfully wrote coverage to ${CLOVER_COVERAGE_LOCATION}/sonar-report.xml`)})();function displayUsage(){console.info(`-l --location: [./coverage]
+	             -s --style: [coverage]||execution`);process.exit(0)}
